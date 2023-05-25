1e58d31f81946d296b50dde2b8ec893a7b8c7452
Mario Antonio Castillo Enríquez <94087243+CastleMart@users.noreply.github.com>
2023-05-25 14:48:53 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/personajes_api_fe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.personajes_api_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/personaje.dart
@@ -0,0 +1,8 @@
+class Personaje {
+  int id;
+  String nombre;
+  String fuerza;
+  String defenza;
+
+  Personaje(this.id, this.nombre, this.fuerza, this.defenza);
+}
