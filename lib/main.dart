65feae25d59c4072d80b813652c322033e422cb0
Mario Antonio Castillo Enríquez <94087243+CastleMart@users.noreply.github.com>
2023-05-25 18:11:05 -0500

    :sparkles: show names

--- lib/main.dart
+++ lib/main.dart
@@ -30,21 +30,55 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi aplicación',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Mi primera prueba'),
-    );
+        title: 'Mi aplicación',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Personajes Fire Emblem"),
+          ),
+          body: FutureBuilder(
+            future: personajes,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView(
+                  children: _listaPersonajes(snapshot.requireData),
+                );
+              } else if (snapshot.hasError) {
+                return Text("Error");
+              }
+
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ));
+  }
+
+  List<Widget> _listaPersonajes(List<Personaje> datos) {
+    List<Widget> personajesWid = [];
+
+    for (var item in datos) {
+      personajesWid.add(Card(
+          child: Column(
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0), child: Text(item.nombre)),
+        ],
+      )));
+    }
+
+    return personajesWid;
   }
 }
 
