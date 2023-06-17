f1da1aeaa49cf1b3193af16e1c6eb274e3b0d038
Mario Antonio Castillo Enríquez <94087243+CastleMart@users.noreply.github.com>
2023-06-16 20:37:50 -0500

    ::art: main

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:personajes_api_fe/views/CrearPersonaje.dart';
 import 'package:personajes_api_fe/views/Favoritos_Page.dart';
 import 'package:personajes_api_fe/views/Home.dart';
 import 'package:personajes_api_fe/views/VerPersonaje.dart';
+import 'package:personajes_api_fe/views/login.dart';
 import 'package:provider/provider.dart';
 
 import 'Herramientas/Botones.dart';
@@ -33,77 +34,22 @@ class MyApp extends StatefulWidget {
 
 //-----------------------------------------------------------------------------------------------------------------------------
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  PersonajeController connect = new PersonajeController();
-  late Future<List<Personaje>> personajes;
-  late List<Personaje> personajesFiltrados;
-  late List<Personaje> personajesLista;
-  int _paginaActual = 0;
-  int idMayor = 0;
-
-  List<Widget> _paginasAdmin = [Home(), CrearPersonaje()];
-  List<Widget> _paginasUsuario = [Home(), FavoritosPage()];
-  List<BottomNavigationBarItem> _navAdmin = [
-    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-    BottomNavigationBarItem(
-        icon: Icon(Icons.create), label: "Ingresar Personaje")
-  ];
-  List<BottomNavigationBarItem> _navUsuario = [
-    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-    BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favoritos"),
-  ];
-
-  late List<BottomNavigationBarItem> _tipoNav;
-  late List<Widget> _paginas;
-
   @override
   void initState() {
     // TODO: implement initState
 
     super.initState();
-
-    ObtenerPersonajes();
-  }
-
-  Future<void> ObtenerPersonajes() async {
-    try {
-      personajes = connect.getPersonajes();
-    } catch (e) {
-      print(e);
-    }
-
-    setState(() {
-      personajes = personajes;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (context.watch<PersonajesProvider>().esAdmin) {
-      _tipoNav = _navAdmin;
-      _paginas = _paginasAdmin;
-    } else {
-      _tipoNav = _navUsuario;
-      _paginas = _paginasUsuario;
-    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mi aplicación',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: _paginas[_paginaActual],
-        bottomNavigationBar: BottomNavigationBar(
-            onTap: (index) {
-              setState(() {
-                _paginaActual = index;
-              });
-            },
-            currentIndex: _paginaActual,
-            items: _tipoNav),
-      ),
+      home: Scaffold(resizeToAvoidBottomInset: false, body: Login()),
     );
   }
 }
